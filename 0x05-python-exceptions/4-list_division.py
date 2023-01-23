cbf73207110a5e0d3a1c17de8e1274a2264a18ae
Alfred-Kobby <alfredternor@gmail.com>
2023-01-23 13:35:49 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,8 +14,8 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("wrong type")
         except ZeroDivisionError:
-             result = 0
-             print("division by 0")
+            result = 0
+            print("division by 0")
         except IndexError:
             result = 0
             print("out of range")
