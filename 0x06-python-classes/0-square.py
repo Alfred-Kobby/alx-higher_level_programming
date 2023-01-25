58e31e018bfa8650a507f93309c9ca5a09c0453c
Alfred-Kobby <alfredternor@gmail.com>
2023-01-25 01:34:10 -0800

    1-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,6 +4,7 @@ Created on Tue May 24 14:21:54 2023
 @author: Alfred Ternor
 """
 
+
 class Square:
     """Represent a square."""
 
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+Created on Tue May 24 14:21:54 2023
+@author: Alfred Ternor
+"""
+
+
 class Square:
     """Represent a square."""
+
     def __init__(self, size):
         """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
+
         self.__size = size
