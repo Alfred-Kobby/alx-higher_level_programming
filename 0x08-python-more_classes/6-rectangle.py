d3f54bdb3609b936e6a09d6ac8a4d6a7a98bc1f9
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-01-30 21:06:54 +0000

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        self.number_of_instances =+ 1
+        self.number_of_instances += 1
 
     @property
     def width(self):
