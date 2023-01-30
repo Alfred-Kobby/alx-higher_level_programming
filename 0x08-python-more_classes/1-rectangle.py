a93611d0b80c01ef25bbab77bee5015e59910f7c
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-01-30 13:42:45 +0000

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+created on Mon Jan 30 17:43:34 2023
+@author: Alfred Ternor
+"""
+
+
+class Rectangle:
+    """Represent a square."""
+    
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Call the function to checking property
+
+        Returns:
+            The width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """check errors and setter for width attribute
+
+        Args:
+            value: Value to checking errors
+
+        Raises:
+            TypeError: Exception if width is not an integer
+            ValueError: Exception if width is less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,1 +1,4 @@
 0-rectangle.py file: an empty class that defines a rectangle
+
+1-rectangle.py file: defines class of a rectangle with height and width
+
