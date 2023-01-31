5a1947919267ff6829893638e1838a1a977b7d01
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-01-31 20:48:43 +0000

    all

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+created on Sun Jan 29 17:43:34 2023
+@author: Alfred Ternor
+"""
+
+
+def print_square(size):
+    """Print a square with the # character.
+
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
