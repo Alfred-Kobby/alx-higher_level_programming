305e623c99ba82094e296049caa9a5c8e6840779
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-06 16:16:01 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Created on Mon Feb 06 16:21:54 2023
+@author: Alfred Ternor
+"""
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+0-lookup.py file: returns the list of available attributes and methods of an object
+
