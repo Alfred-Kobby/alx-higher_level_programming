032f23de52e727aefa9a197b212ea6b3938e0bbe
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-06 16:28:39 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,8 @@ Created on Mon Feb 06 16:21:54 2023
 
 
 class MyList(list):
-    """Represent a List."""
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
         """Print a list in sorted ascending order."""
         print(sorted(self))
