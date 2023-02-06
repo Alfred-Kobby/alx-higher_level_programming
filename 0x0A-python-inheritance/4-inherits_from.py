e844c255edd24db8908cf717c0f07db9fd9753ad
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-06 17:21:55 +0000

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Created on Mon Feb 06 16:21:54 2023
+@author: Alfred Ternor
+"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
