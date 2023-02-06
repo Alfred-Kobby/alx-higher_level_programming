a91c9ceed8c3eacc9e221c9f69a4db1ef28a15c5
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-06 16:52:50 +0000

    1-my_list.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Created on Mon Feb 06 16:21:54 2023
+@author: Alfred Ternor
+"""
+
+
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    return (isinstance(obj, a_class))
