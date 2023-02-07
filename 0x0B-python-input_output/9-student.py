5efcc3f52ffe6cd639e5c32699594b51f98940a4
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-07 20:46:09 +0000

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+created on Tue Feb 07 17:43:34 2023
+@author: Alfred Ternor
+"""
+import json
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """The __init__ method for Student class
+        Args:
+            first_name: first name of student
+            last_name: last name of student
+            age: age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
