c617c228b1feda57652315d3dbc3f174a1a62237
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-07 12:50:01 +0000

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+created on Tue Feb 07 17:43:34 2023
+@author: Alfred Ternor
+"""
+
+
+def write_file(filename="", text=""):
+    """Write a file and prints number of characters.
+        Args:
+            filename : The file to read.
+            text: the text to be written.
+        """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
