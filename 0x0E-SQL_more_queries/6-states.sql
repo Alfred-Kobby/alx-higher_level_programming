05f8abb5489706d69d00917aa3a266f417807dad
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-03-15 17:11:12 +0000

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,8 @@
 -- DDL query to create an user with specific privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Creates the table unique_id.
-CREATE TABLE IF NOT EXISTS `states` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
     `id`   INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );
