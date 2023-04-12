d1a45a984b1ae43867480c5c4b4cd8e982c0281f
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-12 17:52:53 +0000

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,11 @@
 const square = require('./5-square');
 class Square extends square {
   charPrint (c) {
-    if (c === undefine) {
-      c = 'X';
+    if (c === undefined) {
+      this.print();
+    } else {
+        for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
-    this.print(c);
   }
 }
 module.exports = Square;
