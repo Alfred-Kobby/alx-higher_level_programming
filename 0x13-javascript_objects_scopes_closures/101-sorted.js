eede3b22e52713b439757519c4fc302bb25b76d9
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-12 21:13:39 +0000

    100-map.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let myDict = {};
-for (let key in dict) {
-  if (myDict[dict[key]] === undefined) {
-    myDict[dict[key]] = [key];
-  } else {
-    myDict[dict[key]].push(key);
+const myDict = {};
+for (const key in dict) {
+  if (!newDict[dict[key]]) {
+    myDict[dict[key]] = [];
   }
+  myDict[dict[key]].push(key);
 }
 console.log(myDict);
