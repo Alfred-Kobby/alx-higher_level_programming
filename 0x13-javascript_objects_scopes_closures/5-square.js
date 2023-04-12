96555088c8d9907d1e0df39ac34a4a19cabb9be9
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-12 17:27:28 +0000

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super ();
       this.size = size;
   }
 }
-module.exports = Rectangle;
+module.exports = Square;
