239e564194d90673d48f98a3cade47b4b6882d8f
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-12 20:35:03 +0000

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const size = list.length - 1;
-  let reverseArr = [];
+  const reverseArr = [];
   for (let i = size; i >= 0; i--) {
     reverseArr.push(list[i]);
   }
