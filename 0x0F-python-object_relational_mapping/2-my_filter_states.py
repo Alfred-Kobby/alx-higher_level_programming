97fd1ad08594c3c5c6287857b5f03ae68fcf2b8f
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-16 12:17:20 +0000

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+author: Alfred Ternor
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    userInput = args[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    curs = db.cursor()
+    num_rows = curs.execute(f"SELECT * FROM states WHERE states.name = '{userInput}';")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,3 +1,5 @@
 0-select_states.py file: python sql to select all cities
 
 1-filter_states.py file: python sql to filter cities
+
+2-my_filter_states.py file: displays cities where name matches input
