46943a571bb6706859b23b955de110a73cf85f0f
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-16 12:18:09 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=username,
                          passwd=password, db=data, port=3306)
     curs = db.cursor()
-    num_rows = curs.execute(f"SELECT * FROM states WHERE states.name = '{userInput}';")
+    num_rows = curs.execute(f"SELECT * FROM states WHERE states.name = \
+                            '{userInput}';")
     rows = curs.fetchall()
     for row in rows:
         print(row)
