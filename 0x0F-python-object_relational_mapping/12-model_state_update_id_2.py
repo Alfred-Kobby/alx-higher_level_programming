8a277a44aff1d62a762b82d53271e837ee48d1bc
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-16 13:37:48 +0000

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+author: Alfred Ternor
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    user_input = args[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == 2).one()
+    state.name = 'New Mexico'
+    session.commit()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -20,3 +20,5 @@ model_state.py base model for state with ORM
 10-model_state_my_get.py file: prints the state object with the name passed to it as arguement using ORM
 
 11-model_state_insert.py file: adds/insert states object into database using ORMs
+
+12-model_state_update_id_2.py file: update the name of a state object using ORM
