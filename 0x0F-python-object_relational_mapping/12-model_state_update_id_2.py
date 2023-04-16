a0fa1f58433e875501dce5c56edb901d80fe3be9
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-16 13:50:21 +0000

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,13 +10,12 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 5:
+    if len(args) != 4:
         print("Usage: {} username password database_name".format(args[0]))
         exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
-    user_input = args[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, data))
     Session = sessionmaker(bind=engine)
