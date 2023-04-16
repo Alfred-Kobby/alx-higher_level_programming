b92e38640723fddc4f675956a5464593bc750460
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-16 12:43:40 +0000

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+author: Alfred Ternor
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    user_input = args[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    curs = db.cursor()
+    num_rows = curs.execute("SELECT cities.name FROM cities WHERE state_id =\
+                           (SELECT id FROM states WHERE name LIKE BINARY %s)\
+                           ORDER BY cities.id;", (user_input, ))
+    rows = curs.fetchall()
+    i = 1
+    for row in rows:
+        print(row[0], end='')
+        if i < num_rows:
+            print(end=', ')
+        i += 1
+    print()
+    curs.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,9 +1,10 @@
-0-select_states.py file: python sql to select all cities
+0-select_states.py file: python sql to select all states
 
-1-filter_states.py file: python sql to filter cities
+states
+2-my_filter_states.py file: displays states where name matches input
 
-2-my_filter_states.py file: displays cities where name matches input
-
-3-my_safe_filter_states.py file: display cities that matches input but safe from sql injection
+3-my_safe_filter_states.py file: display states that matches input but safe from sql injection
 
 4-cities_by_state.py file: display cities with only one execute call
+
+5-filter_cities.py file: display cities by using state name with one execute call
