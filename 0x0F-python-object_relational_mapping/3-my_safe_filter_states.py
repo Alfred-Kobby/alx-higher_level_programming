b0e26aa2dd2a115c83d8b6ac0165cbc57504ec94
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-16 12:31:16 +0000

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+author: Alfred Ternor
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    user_input = args[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    curs = db.cursor()
+    num_rows = curs.execute("SELECT * FROM states WHERE states.name LIKE \
+                           BINARY %s ORDER BY states.id;".format(user_input))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
+
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -3,3 +3,5 @@
 1-filter_states.py file: python sql to filter cities
 
 2-my_filter_states.py file: displays cities where name matches input
+
+3-my_safe_filter_states.py file: display cities that matches input but safe from sql injection
