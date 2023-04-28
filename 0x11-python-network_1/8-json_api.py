3ef4b3bfa66fa915b6711a14b6fa78e0f97ea207
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-04-28 17:38:41 +0000

    10-my_github.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter."""
+"""Sends a POST request to http://0.0.0.0:5000/search_user with 
+    a given letter."""
 import sys
 import requests
 
@@ -8,9 +9,9 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    my_req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
+        response = my_req.json()
         if response == {}:
             print("No result")
         else:
